8feed35914571c9456f3b5c6c0fd6bccf4e148f7
jorgelfant <jorgel_fant@yahoo.com>
2020-06-26 13:24:07 +0200

    yop

--- src/main/java/com/example/kubernetesWildfly/aplicacion/Aplicacion.java
+++ src/main/java/com/example/kubernetesWildfly/aplicacion/Aplicacion.java
@@ -17,9 +17,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet({ "/aplicacion" })
 public class Aplicacion extends HttpServlet {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 8532332299687592651L;
 
 	@Override
